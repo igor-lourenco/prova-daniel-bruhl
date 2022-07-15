87aa4fc32f5d422e676b9e4b5e1c8cc4bf8ebedb
ilsantos <ilsantos3@stefanini.com>
2022-07-15 11:37:25 -0300

    First commit

---
+++ prova-daniel-bruhl/src/index.js
@@ -0,0 +1,15 @@
+const database = require('../src/io/movies-data.json')
+
+console.log(database)
+
+// Enunciado da nossa prova pequena de hoje!!!
+// Ou você uma lista do filme que o ator Daniel Bruhl participa
+//let moviesFromDanielBruhl = []
+
+// Ou você apenas cospe (console.log) os filmes que o ator Daniel
+// Bruhl participa
+
+// forEach
+// for
+//   let index=0
+//   const object of
